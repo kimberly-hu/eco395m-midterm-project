117cdf92714a2dfe9b27059808f4558d02ce52d1
Kimberly Hu <kimberlyhu@utexas.edu>
2023-10-08 20:11:58 -0500

    clean census data and merge to education

--- code/all_data.py
+++ code/all_data.py
@@ -15,6 +15,7 @@ education_data['District'] = education_data['District'].str[1:]
 education_data['Region'] = education_data['Region'].str[1:]
 education_data['Region_Name'] = education_data['Region_Name'].str.upper()
 
+
 print(education_data.dtypes)
 
 # Clean education data - convert some integers/strings to floats
@@ -30,9 +31,36 @@ for col in education_data.columns[5:30]:
 rates_columns = ['Algebra_Rate', 'Biology_Rate', 'English_Rate', 'US_History_Rate']
 print(education_data[rates_columns].describe())
 
-# Clean education data - remove observations with negative values in rates (i.e. missing data)
+# Clean education data - remove observations with negative values in rates, which should be missing data
 education_data[rates_columns] = education_data[rates_columns].map(lambda x: x if x >= 0 else np.nan)
 education_clean = education_data.dropna(subset = rates_columns)
+
+
+print(census_data.head(5))
+
+census_colnames = {
+    'B19013_001E': 'Median_Household_Income', 
+    'B19083_001E': 'Gini_Index', 
+    'B19113_001E': 'Median_Family_Income',
+                  }
+
+census_data.rename(columns = census_colnames, inplace = True)
+
+print(census_data.dtypes)
+
+# County codes in education data and census data do not match, so we need to merge on county names.
+# Convert county_name in census data into the same format as in education data. 
+census_data['County_Name'] = census_data['NAME'].str.upper()
+census_data['County_Name'] = census_data['County_Name'].str[:-14]
+
+
+# Merge census data to education data
+merged_data = education_clean.merge(census_data[['County_Name', 'Median_Household_Income', 'Gini_Index', 'Median_Family_Income', 'Poverty_Rate']], 
+                                   how = 'left', 
+                                   left_on = 'County_Name',
+                                   right_on = 'County_Name'
+                                  )
+print(merged_data.head(5))
 
 
 
