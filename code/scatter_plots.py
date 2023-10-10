0463811b428b6853c226e3bb6628b9e06e387fe8
Alina-Khindanova <alina_khind@LAPTOP-6FLAU61C>
2023-10-09 22:18:12 -0500

    English rate vs Teaching experience

--- code/scatter_plots.py
+++ code/scatter_plots.py
@@ -18,4 +18,19 @@ plt.ylabel('English_Rate')
 plt.title('Scatter Plot: English_Rate vs. Class_Size_English')
 
 plt.savefig('artifacts/scatter_plots/English_vs_Class_Size_English.png')
+plt.clf()
 
+# English rate and average teaching experience
+
+x = all_data['Teacher_Experience_Average']
+y = all_data['English_Rate']
+
+plt.scatter(x, y, alpha=0.5)
+
+# Add labels and a title
+plt.xlabel('Teacher_Experience_Average')
+plt.ylabel('English_Rate')
+plt.title('Scatter Plot: English_Rate vs. Teacher_Experience_Average')
+
+plt.savefig('artifacts/scatter_plots/English_vs_Teacher_Experience_Average.png')
+plt.clf()
